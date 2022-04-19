6fc90f64e576db8b359f3ce8293a46f9c93b221a
송경희 <kyunghee794613@gmail.com>
2022-04-19 16:41:53 +0900

    [APP-FEAT] app - BottomNav 연결

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/login/SingInFragment.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/login/SingInFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.withssafy.src.login
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,15 +11,24 @@ import com.ssafy.withssafy.config.BaseFragment
 import com.ssafy.withssafy.databinding.FragmentSingInBinding
 
 class SingInFragment : BaseFragment<FragmentSingInBinding>(FragmentSingInBinding::bind,R.layout.fragment_sing_in) {
-
+    private lateinit var signInActivity: SingInActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        signInActivity = context as SingInActivity
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.signInFragmentLoginBtn.setOnClickListener {
+            signInActivity.openFragment(1)
+        }
     }
 
     companion object {
--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/MainActivity.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/MainActivity.kt
@@ -2,11 +2,67 @@ package com.ssafy.withssafy.src.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ssafy.withssafy.R
+import com.ssafy.withssafy.config.BaseActivity
+import com.ssafy.withssafy.databinding.ActivityMainBinding
+import com.ssafy.withssafy.src.main.board.BoardFragment
+import com.ssafy.withssafy.src.main.home.HomeFragment
+import com.ssafy.withssafy.src.main.notification.NotificationFragment
+import com.ssafy.withssafy.src.main.schedule.ScheduleFragment
+import com.ssafy.withssafy.src.main.team.TeamFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    private lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame_layout, HomeFragment())
+            .commit()
+
+        bottomNavigation = binding.mainBottomNav
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame_layout, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.scheduleFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame_layout, ScheduleFragment())
+                        .commit()
+                    true
+                }
+                R.id.boardFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame_layout, BoardFragment())
+                        .commit()
+                    true
+                }
+                R.id.notificationFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame_layout, NotificationFragment())
+                        .commit()
+                    true
+                }
+                R.id.teamFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame_layout, TeamFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+        bottomNavigation.setOnItemReselectedListener { item ->
+            if(bottomNavigation.selectedItemId != item.itemId) {
+                bottomNavigation.selectedItemId = item.itemId
+            }
+        }
     }
 }
