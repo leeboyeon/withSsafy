72248a086045c08cf5a0b70377272125bb26bdbf
최지우 <ji_woo__@naver.com>
2022-04-20 13:29:35 +0900

    [APP-FEAT] app - layout_게시판 fragment

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/board/BoardFragment.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/board/BoardFragment.kt
@@ -1,36 +1,43 @@
 package com.ssafy.withssafy.src.main.board
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import com.ssafy.withssafy.R
+import com.ssafy.withssafy.config.BaseFragment
+import com.ssafy.withssafy.databinding.FragmentBoardBinding
+import com.ssafy.withssafy.src.main.MainActivity
 
 
-class BoardFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
+class BoardFragment : BaseFragment<FragmentBoardBinding>(FragmentBoardBinding::bind, R.layout.fragment_board) {
+    private val TAG = "BoardFragment_ws"
+    private lateinit var mainActivity: MainActivity
 
-        }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_board, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initTabAdapter()
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BoardFragment().apply {
-                arguments = Bundle().apply {
+    private fun initTabAdapter() {
+        val viewPagerAdapter = BoardPageAdapter(this)
+        val tabList = listOf("게시판", "취업", "공지사항")
 
-                }
-            }
+        viewPagerAdapter.addFragment(BoardListFragment())
+        viewPagerAdapter.addFragment(BoardJobFragment())
+        viewPagerAdapter.addFragment(BoardNoticeFragment())
+
+        binding.boardFragmentVp.adapter = viewPagerAdapter
+        TabLayoutMediator(binding.boardFragmentTabLayout, binding.boardFragmentVp) { tab, position ->
+            tab.text = tabList[position]
+        }.attach()
     }
 }
