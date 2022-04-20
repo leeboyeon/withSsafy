16374b76be7a1679e290c551b0078b07df0fb01a
최지우 <ji_woo__@naver.com>
2022-04-20 16:17:57 +0900

    [APP-FEAT] app - 공지사항 layout in 게시판 fragment

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/board/BoardNoticeFragment.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/board/BoardNoticeFragment.kt
@@ -1,23 +1,75 @@
 package com.ssafy.withssafy.src.main.board
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import com.ssafy.withssafy.R
+import com.ssafy.withssafy.config.BaseFragment
+import com.ssafy.withssafy.databinding.FragmentBoardNoticeBinding
+import com.ssafy.withssafy.src.main.MainActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NoticeBoardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BoardNoticeFragment : Fragment() {
+class BoardNoticeFragment : BaseFragment<FragmentBoardNoticeBinding>(FragmentBoardNoticeBinding::bind, R.layout.fragment_board_notice) {
+    private val TAG = "BoardNoticeFragment_ws"
+    private lateinit var mainActivity: MainActivity
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initSsafyRuleDetailToggle()
+    }
+
+    private fun initSsafyRuleDetailToggle() {
+        val attendanceDetail = binding.boardNotiFragmentIvAttendanceDetail
+        val attendance = binding.boardNotiFragmentIvAttendance
+
+        attendanceDetail.setOnClickListener {
+            if(attendance.visibility == View.GONE) { // content가 숨겨져 있는 경우
+                attendanceDetail.setImageResource(R.drawable.ic_arrow_up)
+                attendance.visibility = View.VISIBLE
+            } else if(attendance.visibility == View.VISIBLE) {
+                attendanceDetail.setImageResource(R.drawable.ic_arrow_down)
+                attendance.visibility = View.GONE
+            }
+        }
+
+
+        val lifeRuleDetail = binding.boardNotiFragmentIvLifeRuleDetail
+        val lifeRule = binding.boardNotiFragmentIvLifeRule
+
+        lifeRuleDetail.setOnClickListener {
+            if(lifeRule.visibility == View.GONE) { // content가 숨겨져 있는 경우
+                lifeRuleDetail.setImageResource(R.drawable.ic_arrow_up)
+                lifeRule.visibility = View.VISIBLE
+            } else if(lifeRule.visibility == View.VISIBLE) {
+                lifeRuleDetail.setImageResource(R.drawable.ic_arrow_down)
+                lifeRule.visibility = View.GONE
+            }
+        }
+
+
+        val mileageDetail = binding.boardNotiFragmentIvMileageDetail
+        val mileage = binding.boardNotiFragmentIvMileage
+
+        mileageDetail.setOnClickListener {
+            if(mileage.visibility == View.GONE) { // content가 숨겨져 있는 경우
+                mileageDetail.setImageResource(R.drawable.ic_arrow_up)
+                mileage.visibility = View.VISIBLE
+            } else if(mileage.visibility == View.VISIBLE) {
+                mileageDetail.setImageResource(R.drawable.ic_arrow_down)
+                mileage.visibility = View.GONE
+            }
+        }
+
+    }
 
 }
