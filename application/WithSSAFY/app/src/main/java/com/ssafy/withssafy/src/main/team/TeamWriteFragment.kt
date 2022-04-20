233caa4dae518b40d1c421ac5ca5925625518c1e
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-04-20 14:40:27 +0900

    [APP-FEAT] TeamFragment and TeamWriteFragment Layout

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/team/TeamWriteFragment.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/team/TeamWriteFragment.kt
@@ -5,38 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.ssafy.withssafy.R
 import com.ssafy.withssafy.config.BaseFragment
-import com.ssafy.withssafy.databinding.FragmentTeamBinding
+import com.ssafy.withssafy.databinding.FragmentTeamWriteBinding
 
-
-class TeamFragment : BaseFragment<FragmentTeamBinding>(FragmentTeamBinding::bind,R.layout.fragment_team) {
+class TeamWriteFragment : BaseFragment<FragmentTeamWriteBinding>(FragmentTeamWriteBinding::bind,R.layout.fragment_team_write) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setListener()
-    }
-    private fun setListener(){
-        initButtons()
-    }
-    private fun initButtons(){
-        binding.fragmentTeamWrite.setOnClickListener {
-//            this@TeamFragment.findNavController().navigate(R.id.)
-        }
     }
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TeamFragment().apply {
+            TeamWriteFragment().apply {
                 arguments = Bundle().apply {
-
                 }
             }
     }
