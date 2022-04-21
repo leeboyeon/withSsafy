5a8a29ccb060d5a7df45a076220f0d8a52b6e60a
송경희 <kyunghee794613@gmail.com>
2022-04-21 12:02:44 +0900

    [APP-FEAT] app - Noti List Page Spinner 생성

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/notification/NotiListFragment.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/notification/NotiListFragment.kt
@@ -1,35 +1,49 @@
 package com.ssafy.withssafy.src.main.notification
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.ssafy.withssafy.R
 import com.ssafy.withssafy.config.BaseFragment
 import com.ssafy.withssafy.databinding.FragmentNotiListBinding
+import com.ssafy.withssafy.src.main.MainActivity
 
 
 class NotiListFragment : BaseFragment<FragmentNotiListBinding>(FragmentNotiListBinding::bind, R.layout.fragment_noti_list) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
+    private lateinit var mainActivity: MainActivity
 
-        }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        initSpinner()
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NotiListFragment().apply {
-                arguments = Bundle().apply {
+    private fun initSpinner() {
+        val spin = binding.notiListSp
+        spin.apply {
+            adapter = ArrayAdapter.createFromResource(requireContext(), R.array.noti, android.R.layout.simple_spinner_dropdown_item)
+        }
+        // spin.selectedItem.toString()
+        spin.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
-                }
             }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+
     }
+
 }
