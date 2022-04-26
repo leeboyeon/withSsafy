121f9e7d31c7e9d3e92957fa40ec09358145f647
송경희 <kyunghee794613@gmail.com>
2022-04-26 13:25:16 +0900

    [APP-FEAT] app - 회원가입 유효성 검사 & 반정보 아이디 찾기

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/dto/User.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/dto/User.kt
@@ -12,4 +12,5 @@ data class User(
 ) {
     constructor() : this(id = 0, name = "", userId = "", password = "", studentId = "", classRoomId = 0, state = 0, deviceToken = "")
     constructor(id: Int) : this(id = id, name = "", userId = "", password = "", studentId = "", classRoomId = 0, state = 0, deviceToken = "")
+    constructor(name: String, userId: String, password: String, studentId: String, classRoomId: Int) : this(id = 0, name = name, userId = userId, password = password, studentId = studentId, classRoomId = classRoomId, state = 0, deviceToken = "")
 }
--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/login/SignUpFragment.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/login/SignUpFragment.kt
@@ -2,18 +2,30 @@ package com.ssafy.withssafy.src.login
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.ssafy.withssafy.R
 import com.ssafy.withssafy.config.BaseFragment
 import com.ssafy.withssafy.databinding.FragmentSignUpBinding
+import com.ssafy.withssafy.src.dto.User
 import kotlinx.coroutines.runBlocking
+import java.util.regex.Pattern
 
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding::bind, R.layout.fragment_sign_up) {
     private lateinit var signInActivity: SingInActivity
+
+    private var gen = ""
+    private var area = ""
+    private var classNum = ""
+    private var classRoomId = -1
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -26,16 +38,33 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding
 
         getClassRoomListInit()
         initSpinner()
-    }
+        initListeners()
 
-    private fun initListener() {
-
+        binding.signUpFragmentBtnSignUp.setOnClickListener {
+            var user = isAvailable()
+            if(user != null) {
+                //signUp(user)
+            } else {
+                Toast.makeText(requireContext(), "입력 값을 다시 확인해 주세요", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     private fun getClassRoomListInit() {
         runBlocking {
             userViewModel.getClassRoomList()
         }
+    }
+
+    /**
+     * TextInputEditText listener 등록
+     */
+    private fun initListeners() {
+        binding.signUpFragmentEtId.addTextChangedListener(TextFieldValidation(binding.signUpFragmentEtId))
+        binding.signUpFragmentEtPw.addTextChangedListener(TextFieldValidation(binding.signUpFragmentEtPw))
+        binding.signUpFragmentEtName.addTextChangedListener(TextFieldValidation(binding.signUpFragmentEtName))
+        binding.signUpFragmentEtStuId.addTextChangedListener(TextFieldValidation(binding.signUpFragmentEtStuId))
+        selectSpinner()
     }
 
     /**
@@ -69,6 +98,201 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding
         }
         classSpin.apply {
             adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, newClassList.toList())
+        }
+    }
+
+    private fun isAvailable() : User? {
+        if((validatedId() && validatedPw() && validatedName() && validatedStuId()) || (gen != "" || area != "" || classNum != "")) {
+            val id = binding.signUpFragmentEtId.text.toString()
+            val pw = binding.signUpFragmentEtPw.text.toString()
+            val name = binding.signUpFragmentEtName.text.toString()
+            val stuId = binding.signUpFragmentEtStuId.text.toString()
+            return User(id, pw, name, stuId, classRoomId)
+        } else {
+            return null
+        }
+    }
+
+    /**
+     * 입력된 id 유효성 검사
+     * @return id 중복 통과 시 true 반환
+     */
+    private fun validatedId() : Boolean {
+        val inputId = binding.signUpFragmentEtId.text.toString()
+
+        if (inputId.trim().isEmpty()) {
+            binding.signUpFragmentTilId.error = "Required Field"
+            binding.signUpFragmentEtId.requestFocus()
+            return false
+        } else if(!Pattern.matches("^[가-힣ㄱ-ㅎa-zA-Z0-9._-]{4,20}\$", inputId)) {
+            binding.signUpFragmentTilId.error = "아이디 형식을 확인해주세요.(특수 문자 제외)"
+            binding.signUpFragmentEtId.requestFocus()
+            return false
+        } else {
+            binding.signUpFragmentTilId.isErrorEnabled = false
+            return true
+        }
+    }
+
+    /**
+     * 입력된 password 유효성 검사
+     * @return 유효성 검사 통과 시 true 반환
+     */
+    private fun validatedPw() : Boolean {
+        val inputPw = binding.signUpFragmentEtPw.text.toString()
+
+        if(inputPw.trim().isEmpty()){   // 값이 비어있으면
+            binding.signUpFragmentTilPw.error = "Required Field"
+            binding.signUpFragmentEtPw.requestFocus()
+            return false
+        } else if(!Pattern.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[\$@!%*#?&]).{8,50}.\$", inputPw)) {
+            binding.signUpFragmentTilPw.error = "비밀번호 형식을 확인해주세요."
+            binding.signUpFragmentEtPw.requestFocus()
+            return false
+        }
+        else {
+            binding.signUpFragmentTilPw.isErrorEnabled = false
+            return true
+        }
+    }
+
+    /**
+     * 입력된 name 빈 칸 체크
+     * @return 통과 시 true 반환
+     */
+    private fun validatedName() : Boolean{
+        val inputNickname = binding.signUpFragmentEtName.text.toString()
+
+        if(inputNickname.trim().isEmpty()){
+            binding.signUpFragmentTilName.error = "Required Field"
+            binding.signUpFragmentEtName.requestFocus()
+            return false
+        } else {
+            binding.signUpFragmentTilName.error = null
+            return true
+        }
+    }
+
+    /**
+     * 입력된 학번 빈 칸 체크
+     * @return 통과 시 true 반환
+     */
+    private fun validatedStuId() : Boolean{
+        val inputNickname = binding.signUpFragmentEtStuId.text.toString()
+
+        if(inputNickname.trim().isEmpty()){
+            binding.signUpFragmentTilStuId.error = "Required Field"
+            binding.signUpFragmentEtStuId.requestFocus()
+            return false
+        } else {
+            binding.signUpFragmentTilStuId.error = null
+            return true
+        }
+    }
+
+    private fun selectSpinner() {
+        val genSpin= binding.signUpFragmentSpinnerStuGen
+        val areaSpin = binding.signUpFragmentSpinnerArea
+        val classSpin = binding.signUpFragmentSpinnerClass
+
+        genSpin.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> {
+                        gen = ""
+                    }
+                    else -> {
+                        gen = genSpin.selectedItem.toString()
+
+                        if(gen != "" && area != "" && classNum != "") {
+                            for(i in userViewModel.classRommList.value!!) {
+                                if(i.generation == gen && i.area == area && i.classDescription == classNum) {
+                                    classRoomId = i.id
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+
+        areaSpin.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> {
+                        area = ""
+                    }
+                    else -> {
+                        area = areaSpin.selectedItem.toString()
+
+                        if(gen != "" && area != "" && classNum != "") {
+                            for(i in userViewModel.classRommList.value!!) {
+                                if(i.generation == gen && i.area == area && i.classDescription == classNum) {
+                                    classRoomId = i.id
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+
+        classSpin.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> {
+                        classNum = ""
+                    }
+                    else -> {
+                        classNum = classSpin.selectedItem.toString()
+
+                        if(gen != "" && area != "" && classNum != "") {
+                            for(i in userViewModel.classRommList.value!!) {
+                                if(i.generation == gen && i.area == area && i.classDescription == classNum) {
+                                    classRoomId = i.id
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+    }
+
+    inner class TextFieldValidation(private val view: View): TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            when(view.id){
+                R.id.signUpFragment_etId-> {
+                    validatedId()
+                }
+                R.id.signUpFragment_etPw -> {
+                    validatedPw()
+                }
+                R.id.signUpFragment_etName -> {
+                    validatedName()
+                }
+                R.id.signUpFragment_etStuId -> {
+                    validatedStuId()
+                }
+            }
         }
     }
 }
