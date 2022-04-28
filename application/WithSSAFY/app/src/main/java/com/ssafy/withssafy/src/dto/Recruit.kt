8a8571f1b2607b281eda243f026ea7959b177fd1
송경희 <kyunghee794613@gmail.com>
2022-04-28 15:01:42 +0900

    [APP-FEAT] app - Recruit, RecruitLike Dto 생성

---
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/dto/Recruit.kt
@@ -0,0 +1,19 @@
+package com.ssafy.withssafy.src.dto
+
+data class Recruit(
+    val career: String,
+    val company: String,
+    val education: String,
+    val employType: String,
+    val endDate: String,
+    val id: Int,
+    val job: String,
+    val location: String,
+    val preferenceDescription: String,
+    val salary: String,
+    val startDate: String,
+    val taskDescription: String,
+    val userId: Int,
+    val welfare: String,
+    val workingHours: String
+)
---
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/dto/RecruitLike.kt
@@ -0,0 +1,7 @@
+package com.ssafy.withssafy.src.dto
+
+data class RecruitLike(
+    val id: Int,
+    val recruitId: String,
+    val userId: String
+)
