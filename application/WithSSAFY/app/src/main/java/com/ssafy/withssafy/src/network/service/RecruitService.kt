fd2ea2200003df1999fb5d4621b8be6f7312eb27
송경희 <kyunghee794613@gmail.com>
2022-04-28 15:09:10 +0900

    [APP-FEAT] app - 채용 공고 Service 메서드들 생성

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/network/service/RecruitService.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/network/service/RecruitService.kt
@@ -1,4 +1,24 @@
 package com.ssafy.withssafy.src.network.service
 
+import com.ssafy.withssafy.src.dto.Recruit
+import com.ssafy.withssafy.src.dto.RecruitLike
+import com.ssafy.withssafy.util.RetrofitUtil
+import retrofit2.http.Body
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 class RecruitService {
+    suspend fun selectRecruitAll() = RetrofitUtil.recruitApi.selectRecruitAll()
+
+    suspend fun insertRecruit(recruitDto : Recruit) = RetrofitUtil.recruitApi.insertRecruit(recruitDto)
+
+    suspend fun updateRecruit(recruitDto : Recruit) = RetrofitUtil.recruitApi.updateRecruit(recruitDto)
+
+    suspend fun selectRecruitById(id: Int) = RetrofitUtil.recruitApi.selectRecruitById(id)
+
+    suspend fun deleteRecruitById(id: Int) = RetrofitUtil.recruitApi.deleteRecruitById(id)
+
+    suspend fun likeRecruit(recruitId : Int, userId : Int) = RetrofitUtil.recruitApi.likeRecruit(recruitId, userId)
+
+    suspend fun likeCancelRecruit(recruitLikeDto : RecruitLike) = RetrofitUtil.recruitApi.likeCancelRecruit(recruitLikeDto)
 }
