37164212a5b02f9fca4f1921f12b796c8c7f74bd
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-04-29 16:47:36 +0900

    [APP-FEAT] study Delete

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/team/TeamAdapter.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/team/TeamAdapter.kt
@@ -1,24 +1,34 @@
 package com.ssafy.withssafy.src.main.team
 
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Filter
-import android.widget.Filterable
+import android.widget.*
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.withssafy.R
+import com.ssafy.withssafy.config.ApplicationClass
 import com.ssafy.withssafy.databinding.ItemTeamBinding
 import com.ssafy.withssafy.src.dto.study.Study
+import com.ssafy.withssafy.src.network.service.StudyService
+import com.ssafy.withssafy.src.viewmodel.TeamViewModel
+import kotlinx.coroutines.runBlocking
 
 private const val TAG = "TeamAdapter"
-class TeamAdapter : RecyclerView.Adapter<TeamAdapter.TeamViewHolder>() , Filterable{
+class TeamAdapter(val context: Context) : RecyclerView.Adapter<TeamAdapter.TeamViewHolder>() , Filterable{
     var list = mutableListOf<Study>()
     var filteredList = list
     inner class TeamViewHolder(private val binding : ItemTeamBinding):RecyclerView.ViewHolder(binding.root){
         fun bind(study: Study){
             binding.study = study
+            if(study.user!!.id == ApplicationClass.sharedPreferencesUtil.getUser().id){
+                binding.fragmentTeamMoreBtn.visibility = View.VISIBLE
+            }else{
+                binding.fragmentTeamMoreBtn.visibility = View.GONE
+            }
             binding.executePendingBindings()
         }
     }
@@ -33,20 +43,51 @@ class TeamAdapter : RecyclerView.Adapter<TeamAdapter.TeamViewHolder>() , Filtera
             itemView.setOnClickListener {
                 itemClickListener.onClick(it,position,filteredList[position].id)
             }
+            var more = itemView.findViewById<ImageView>(R.id.fragment_team_moreBtn)
+            more.setOnClickListener {
+                val popup = PopupMenu(context,more)
+                MenuInflater(context).inflate(R.menu.popup_menu_writer,popup.menu)
+                popup.show()
+                popup.setOnMenuItemClickListener {
+                    when(it.itemId){
+                        R.id.modify ->{
+                            modifyClickListener.onClick(position,list[position].id)
+                            return@setOnMenuItemClickListener true
+                        }
+                        R.id.delete -> {
+                            deleteClickListener.onClick(position,list[position].id)
+                            return@setOnMenuItemClickListener true
+                        }else ->
+                            return@setOnMenuItemClickListener false
+                    }
+                }
+            }
         }
     }
-
     override fun getItemCount(): Int {
         return filteredList.size
     }
     interface ItemClickListener{
         fun onClick(view: View, position: Int, id: Int)
     }
-
+    interface DeleteClickListener{
+        fun onClick(position: Int, id:Int)
+    }
+    interface ModifyClickListener{
+        fun onClick(position:Int, id:Int)
+    }
+    private lateinit var deleteClickListener : DeleteClickListener
     private lateinit var itemClickListener : ItemClickListener
+    private lateinit var modifyClickListener: ModifyClickListener
 
     fun setItemClickListener(itemClickListener: ItemClickListener){
         this.itemClickListener = itemClickListener
+    }
+    fun setDeleteClickListener(deleteClickListener: DeleteClickListener) {
+        this.deleteClickListener = deleteClickListener
+    }
+    fun setModifyClickListener(modifyClickListener: ModifyClickListener){
+        this.modifyClickListener = modifyClickListener
     }
 
     override fun getFilter(): Filter {
