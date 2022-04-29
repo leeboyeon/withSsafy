250ca98b2ae5271297aecfa7c98a6a9f6c4c1983
송경희 <kyunghee794613@gmail.com>
2022-04-29 10:34:30 +0900

    [APP-FEAT] app - JobAdapter Databinding 연결

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/board/JobAdapter.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/board/JobAdapter.kt
@@ -1,17 +1,20 @@
 package com.ssafy.withssafy.src.main.board
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.withssafy.R
 import com.ssafy.withssafy.databinding.ItemJobBinding
+import com.ssafy.withssafy.src.dto.Recruit
 
 class JobAdapter : RecyclerView.Adapter<JobAdapter.JobViewHolder>(){
-    var list = mutableListOf<String>()
+    var list = mutableListOf<Recruit>()
     inner class JobViewHolder(private val binding:ItemJobBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(){
-
+        fun bind(recruit : Recruit){
+            binding.recruit = recruit
+            binding.executePendingBindings()
         }
     }
 
@@ -21,8 +24,21 @@ class JobAdapter : RecyclerView.Adapter<JobAdapter.JobViewHolder>(){
 
     override fun onBindViewHolder(holder: JobViewHolder, position: Int) {
         holder.apply {
-            bind()
+            bind(list[position])
+            itemView.setOnClickListener {
+                itemClickListener.onClick(it,position,list[position].id)
+            }
         }
+    }
+
+    interface ItemClickListener{
+        fun onClick(view: View, position: Int, id: Int)
+    }
+
+    private lateinit var itemClickListener : ItemClickListener
+
+    fun setItemClickListener(itemClickListener: ItemClickListener){
+        this.itemClickListener = itemClickListener
     }
 
     override fun getItemCount(): Int {
