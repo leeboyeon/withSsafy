d21d3af32ad848417aa083c5ef6e2ee0c2fec811
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-05-02 22:34:18 +0900

    [APP-FEAT] class Currculum Write Fragment

---
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/schedule/CurriculumWriteFragment.kt
@@ -0,0 +1,32 @@
+package com.ssafy.withssafy.src.main.schedule
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.withssafy.R
+import com.ssafy.withssafy.config.BaseFragment
+import com.ssafy.withssafy.databinding.FragmentCurriculumWriteBinding
+
+class CurriculumWriteFragment : BaseFragment<FragmentCurriculumWriteBinding>(FragmentCurriculumWriteBinding::bind,R.layout.fragment_curriculum_write) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CurriculumWriteFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
