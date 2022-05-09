f6a76f081bab1ad095ce7f1a8c50b00d2890ef11
최주은 <ilutoo@naver.com>
2022-05-09 14:37:59 +0900

    [BACK-FIX] 일부 요구사항 수정

--- server/withssafy/src/main/java/com/ssafy/withssafy/dto/sbcomment/SbCommentDto.java
+++ server/withssafy/src/main/java/com/ssafy/withssafy/dto/sbcomment/SbCommentDto.java
@@ -2,6 +2,7 @@ package com.ssafy.withssafy.dto.sbcomment;
 
 import com.ssafy.withssafy.dto.studyboard.StudyBoardResponse;
 import com.ssafy.withssafy.dto.user.UserDto;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 
@@ -9,9 +10,10 @@ import lombok.ToString;
 @ToString
 public class SbCommentDto {
     private Long id;
-    private StudyBoardResponse studyBoard;
-    private UserDto user;
+    private Long boardId;
+    private Long userId;
     private Long parent;
     private String content;
+    @ApiModelProperty(hidden = true)
     private String write_dt;
 }
