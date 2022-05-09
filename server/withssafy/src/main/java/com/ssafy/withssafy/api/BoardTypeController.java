42e2c26debbdb6dc2ff1e6c496a0c5299768def8
김수호 <is_null@kakao.com>
2022-05-09 10:45:18 +0900

    [BACK-FEAT] 게시판 생성 수정 삭제

--- server/withssafy/src/main/java/com/ssafy/withssafy/api/BoardTypeController.java
+++ server/withssafy/src/main/java/com/ssafy/withssafy/api/BoardTypeController.java
@@ -1,12 +1,12 @@
 package com.ssafy.withssafy.api;
 
+import com.ssafy.withssafy.dto.board.BoardTypeRequest;
 import com.ssafy.withssafy.service.board.BoardTypeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,4 +19,24 @@ public class BoardTypeController {
     public ResponseEntity<?> getBoardTypes() {
         return new ResponseEntity<>(boardTypeService.getBoardTypes(), HttpStatus.OK);
     }
+
+    @PostMapping()
+    public ResponseEntity<?> addBoardType(@RequestBody BoardTypeRequest boardTypeRequest){
+        boardTypeService.addBoardType(boardTypeRequest);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> modifyBoardTypeById(@RequestBody BoardTypeRequest boardTypeRequest, @PathVariable("id") Long id){
+        boardTypeService.modifyBoardTypeById(boardTypeRequest, id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeBoardTypeById(@PathVariable("id") Long id){
+        boardTypeService.removeBoardTypeById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
 }
---
+++ server/withssafy/src/main/java/com/ssafy/withssafy/dto/board/BoardTypeRequest.java
@@ -0,0 +1,8 @@
+package com.ssafy.withssafy.dto.board;
+
+import lombok.Data;
+
+@Data
+public class BoardTypeRequest {
+    private String type;
+}
