ac127c87075c8a5e3c9abf006167c320d461b317
pdj1649 <pdj1649@gmail.com>
2022-05-10 14:52:56 +0900

    [BACK-FEAT] 특정 날짜 일정 조회 api

--- server/withssafy/src/main/java/com/ssafy/withssafy/repository/ScheduleRepository.java
+++ server/withssafy/src/main/java/com/ssafy/withssafy/repository/ScheduleRepository.java
@@ -21,4 +21,12 @@ public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
             "where c1.id = :classRoomId and c1.generation = c2.generation and\n" +
             "c2.class_description = \"전체\" and (c1.area = c2.area or c2.area = \"전체\"));",nativeQuery = true)
     List<Schedule> findAllGenerationNotice(Long classRoomId);
+
+    @Query(value = "select s.* from tbl_schedule s\n" +
+            "where s.class_room_id in (select c2.id\n" +
+            "from tbl_classroom c1, tbl_classroom c2\n" +
+            "where c1.id = :id and c1.generation = c2.generation and\n" +
+            "c2.class_description = \"전체\" and (c1.area = c2.area or c2.area = \"전체\"))\n" +
+            "and date_format(s.start_date,'%Y-%m-%d') = :day",nativeQuery = true)
+    List<Schedule> findAllGenerationNoticyByDay(Long id, LocalDate day);
 }
