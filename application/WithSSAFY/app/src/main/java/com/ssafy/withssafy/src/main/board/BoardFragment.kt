fea8ace96d953d87eb60bb746b70ad457877b266
최지우 <ji_woo__@naver.com>
2022-05-11 15:53:10 +0900

    [APP-FEAT] 사용자가 댓글 단 글 조회 기능

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/board/BoardFragment.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/board/BoardFragment.kt
@@ -2,6 +2,7 @@ package com.ssafy.withssafy.src.main.board
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
