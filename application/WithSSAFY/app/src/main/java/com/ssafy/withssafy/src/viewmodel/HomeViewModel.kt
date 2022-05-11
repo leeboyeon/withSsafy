9603de37c026d13d18bbd85a0f7c999dfac0a494
최지우 <ji_woo__@naver.com>
2022-05-11 11:21:51 +0900

    [APP-FEAT] 신고 내역 조회 기능

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/viewmodel/HomeViewModel.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/viewmodel/HomeViewModel.kt
@@ -1,10 +1,19 @@
 package com.ssafy.withssafy.src.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.withssafy.src.dto.board.Board
+import com.ssafy.withssafy.src.dto.report.Report
+import com.ssafy.withssafy.src.network.service.BoardService
+import com.ssafy.withssafy.src.network.service.ReportService
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class HomeViewModel : ViewModel(){
+    private val TAG = "HomeViewModel_ws"
     private val _bannerItemList: MutableLiveData<List<Int>> = MutableLiveData()
     private val _currentPosition: MutableLiveData<Int> = MutableLiveData()
 
@@ -28,4 +37,37 @@ class HomeViewModel : ViewModel(){
 
     fun getcurrentPosition() = currentPosition.value
 
+
+    /**
+     * 신고 내용 조회
+     */
+    private val _reportList = MutableLiveData<MutableList<Report>>()
+
+    val reportList : LiveData<MutableList<Report>>
+        get() = _reportList
+
+    private fun setReportList(list: MutableList<Report>) = viewModelScope.launch {
+        _reportList.value = list
+    }
+
+    suspend fun getReportList() {
+        try {
+            val response = ReportService().getReportList()
+
+            viewModelScope.launch {
+                if(response.isSuccessful) {
+                    val res = response.body()
+                    if(res != null) {
+                        setReportList(res as MutableList<Report>)
+                    } else {
+                        Log.d(TAG, "getReportList: $response", )
+                    }
+                } else {
+                    Log.e(TAG, "getReportList: 통신 실패", )
+                }
+            }
+        } catch (e: HttpException) {
+            Log.e(TAG, "getReportList ${e.message()}", )
+        }
+    }
 }
