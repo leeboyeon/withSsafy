1cec1a44dd0c04996a62d81a2e81b22acf0a4216
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-05-11 13:16:26 +0900

    [APP-FEAT] 전체일정 수정 및 삭제 + 전체일정 수정 및 삭제 권한자 허용

--- application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/schedule/DayDetailAdapter.kt
+++ application/WithSSAFY/app/src/main/java/com/ssafy/withssafy/src/main/schedule/DayDetailAdapter.kt
@@ -1,15 +1,19 @@
 package com.ssafy.withssafy.src.main.schedule
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.withssafy.R
+import com.ssafy.withssafy.config.ApplicationClass
 import com.ssafy.withssafy.databinding.ItemCalendarDetailBinding
 import com.ssafy.withssafy.src.dto.Schedule
 
 class DayDetailAdapter() : RecyclerView.Adapter<DayDetailAdapter.DayViewHolder>() {
     var list = mutableListOf<Schedule>()
+    var userId = ApplicationClass.sharedPreferencesUtil.getUser().id
+    var studentId = ApplicationClass.sharedPreferencesUtil.getUser().studentId
     inner class DayViewHolder(private val binding:ItemCalendarDetailBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(schedule:Schedule){
             binding.schedule = schedule
@@ -24,10 +28,27 @@ class DayDetailAdapter() : RecyclerView.Adapter<DayDetailAdapter.DayViewHolder>(
     override fun onBindViewHolder(holder: DayViewHolder, position: Int) {
         holder.apply {
             bind(list[position])
+            if(studentId == null){
+                //admin
+                itemView.setOnClickListener {
+                    itemClickListener.onClick(it,position,list[position].id)
+                }
+            }
+
         }
     }
 
     override fun getItemCount(): Int {
         return list.size
+    }
+
+    interface ItemClickListener{
+        fun onClick(view: View, position: Int, scheduleId:Int)
+    }
+
+    private lateinit var itemClickListener : ItemClickListener
+
+    fun setItemClickListener(itemClickListener: ItemClickListener){
+        this.itemClickListener = itemClickListener
     }
 }
