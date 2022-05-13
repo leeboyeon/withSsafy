78d06786737fc39df4b904e6c71b7e354c388602
김수호 <is_null@kakao.com>
2022-05-13 16:18:15 +0900

    [BACK-MODIFY] 게시물 작성 시간 역정렬

--- server/withssafy/src/main/java/com/ssafy/withssafy/repository/board/BoardRepository.java
+++ server/withssafy/src/main/java/com/ssafy/withssafy/repository/board/BoardRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.withssafy.repository.board;
 
 import com.ssafy.withssafy.entity.Board;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,15 +9,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardCustomRepository {
-    List<Board> findAllByTypeId(Long typeId);
+    List<Board> findAllByTypeId(Long typeId, Sort sort);
 
     @Query("SELECT board FROM Board board WHERE board.likes.size >= 10 ORDER BY board.likes.size DESC")
-    List<Board> findAllHotBoards();
+    List<Board> findAllHotBoards(Sort sort);
 
     @Query("SELECT board " +
             "FROM Board board LEFT JOIN " +
             "LikeManagement likeManagement " +
             "ON board.id = likeManagement.board.id " +
             "WHERE likeManagement.user.id = :userId")
-    List<Board> findAllLikedBoardByUserId(@Param(value = "userId") Long userId);
+    List<Board> findAllLikedBoardByUserId(@Param(value = "userId") Long userId, Sort sort);
 }
